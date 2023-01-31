bf17692263e465695afc0461f30b7d400cd0f4c5
991567775 <991567775@qq.com>
2023-01-31 14:14:01 +0800

    springboot升级到3

--- s-core/src/main/java/cn/ezeyc/edpbase/http/PostFilter.java
+++ s-core/src/main/java/cn/ezeyc/edpbase/http/PostFilter.java
@@ -9,6 +9,9 @@ import jakarta.servlet.*;
 import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+import org.springframework.web.multipart.support.StandardMultipartHttpServletRequest;
+
 import java.io.IOException;
 
 /**
@@ -27,7 +30,20 @@ public class PostFilter implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) {
         try {
-            filterChain.doFilter(servletRequest, servletResponse);
+            if(servletRequest.getContentType().contains("multipart/form-data")){
+                filterChain.doFilter(servletRequest, servletResponse);
+            }else{
+                ServletRequest requestWrapper = null;
+                if(servletRequest instanceof HttpServletRequest) {
+                    requestWrapper = new RequestWrapper((HttpServletRequest) servletRequest);
+                }
+                if(requestWrapper == null) {
+                    filterChain.doFilter(servletRequest, servletResponse);
+                } else {
+                    filterChain.doFilter(requestWrapper, servletResponse);
+                }
+            }
+
         } catch (IOException e) {
             Http.returnJson((HttpServletResponse) servletResponse,ResultBody.failed(ResultEnum.verify).setMessage(e.getCause().getMessage()));
         } catch (ServletException e) {
